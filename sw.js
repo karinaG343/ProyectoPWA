023e0edfa64cf4c373e5c93791df6ca3087ab55a
karinaG343 <83309631+karinaG343@users.noreply.github.com>
2022-12-02 17:44:29 -0600

    Avnace sw

--- sw.js
+++ sw.js
@@ -1,71 +1,48 @@
-//asignar nombre y version de cache
-const CACHE_NAME = 'v1_cache_awp';
-
-//configurar los archivos del cache
-
-var urlToCache = [
-    'css/style.css',
-    'images/1.png',
-    'images/2.png',
-    'images/3.png',
-    'images/4.png',
-    'images/5.png',
-    'images/6.png',
-    'images/7.png',
-    'images/facebook.png',
-    'images/instagram.png',
-    'images/twiter.png',
-];
-
-// Evento install
-// Instalacion del service worker y guardar en cache los recursos estaticos
 self.addEventListener('install', e =>{
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-              .then(cache => {
-                    return cache.addAll(urlToCache)
-                           .then(() => {
-                                self.skipWaiting();
-                           })
-                 })
-                 .catch(err => {
-                    console.log('No se ha registrado el cache', err);
-                   })
-    )
+  const cacheProm = caches.open('cache-v1')
+      .then(cache => {
+          return cache.addAll([
+            'css/style.css',
+            'images/1.png',
+            'images/2.png',
+            'images/3.png',
+            'images/4.png',
+            'images/5.png',
+            'images/6.png',
+            'images/7.png',
+            'images/facebook.png',
+            'images/instagram.png',
+            'images/twiter.png',
+            'videos/1.mp4',
+            'videos/2.mp4',
+            'videos/3.mp4',
+            'index.html',
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+          ])
+          
+      });
+  e.waitUntil(cacheProm);
 });
 
-//Evento activate
-self.addEventListener('activate', e =>{
-    const cacheWhiteList = [CACHE_NAME];
-    e.waitUntil(
-        caches.keys()
-              .then(cacheNames => {
-                return Promise.all(
-                    cacheNames.map(cacheName => {
-                        if(cacheWhiteList.indexOf(cacheName) === -1){
-                            // Borrar los elementos q no se necesitan
-                            return caches.delete(cacheName);
-                        }
-                    })
-                );
-              })
-              .then(() =>{
-                //activar la cache
-                self.clients.claim();
-              })
-    );
-});
+self.addEventListener('fetch', e =>{
+  //cache with network fallback
+  const respuesta = caches.match( e.request )
+      .then ( res => {
+          if ( res ) return res;
+          //no existe el archivo
+          //tengo que ir a la web
+          console.log('No existe', e.request.url);
+          return fetch( e.request ).then ( newResp => {
+              caches.open('cache-v1')
+                  .then( cache => {
+                      cache.put( e.request, newResp);
+                  }
 
-///Evento fetch
-self.addEventListener("fetch", event => {
-	event.respondWith(
-		caches.open(CACHE_NAME).then(cache => {
-			return cache.match(event.request).then(response => {
-				return response || fetch(event.request).then(networkResponse => {
-					cache.put(event.request, networkResponse.clone());
-					return networkResponse;
-				});
-			})
-		})
-	);
+                  )
+              return newResp.clone;
+          });
+      });
+      e.respondWith(respuesta);
+  //only cache
+  //e.respondWith( caches.match(e.request));
 });
